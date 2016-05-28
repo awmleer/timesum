ec4fe1fabe9050f65b163e4e7974c77f63736f5e
qzhsjz <qzhsjz@gmail.com>
2016-05-29 00:28:28 +0800

    ChangePwd

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -71,6 +71,7 @@ app.controller("ctrl_header",function($scope,$rootScope,$http,$state,$location) 
 
 app.controller("ctrl_userinfo",function($scope,$rootScope,$http,$stateParams) {
     $scope.NameChgDivVisible = false;
+    $scope.PwdChgDivVisible = false;
     //$rootScope.get_userinfo();
     $rootScope.userinfo = {
         "uid": 1,
@@ -80,6 +81,9 @@ app.controller("ctrl_userinfo",function($scope,$rootScope,$http,$stateParams) {
     $scope.userinfo = $rootScope.userinfo;
     $scope.NameChgVisibleToogle = function () {
         $scope.NameChgDivVisible = !$scope.NameChgDivVisible;
+    }
+    $scope.PwdChgVisibleToogle = function () {
+        $scope.PwdChgDivVisible = !$scope.PwdChgDivVisible;
     }
     $scope.CommitNameChg = function () {
         $http({
@@ -98,7 +102,30 @@ app.controller("ctrl_userinfo",function($scope,$rootScope,$http,$stateParams) {
         }).error(function () {
             alert("操作失败");
         });
-    }
+        $scope.CommitPwdChg = function () {
+            if ($scope.pwdchg.newpwd == $scope.pwdchg.newpwdcfm) {
+                $http({
+                    url: 'api/changepwd',
+                    method: 'post',
+                    headers: {'Content-Type': 'application/json'},
+                    data: JSON.stringify({
+                        uid: $rootScope.userinfo.uid,
+                        name:$rootScope.userinfo.name
+                    })
+                }).success(function (data) {
+                    if (data == 'success') {
+                        alert("操作成功！");
+                        $scope.NameChgDivVisible = false;
+                    }
+                }).error(function () {
+                    alert("操作失败");
+                });
+            }
+            else {
+                alert("两次输入的密码不一致！");
+            }
+        }
+        }
 });
 
 
