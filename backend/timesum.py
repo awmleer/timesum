f047fb8c3a8c31e1bbe6141f0c4d07783d651bc4
qzhsjz <qzhsjz@gmail.com>
2016-06-07 21:44:31 +0800

    解析换到后端

--- backend/timesum.py
+++ backend/timesum.py
@@ -485,7 +485,7 @@ def time_input():
         for tblocks in singledata['timeblocks']:
             tbstr += str(tblocks['status'])
         singledata['timeblocks'] = tbstr
-        del singledata['day_in_week']
+        del singledata['date']['day_in_week']
 
 
     text['aid'] = int(text['aid'])
